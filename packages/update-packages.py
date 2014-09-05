1a7135df04b5c968e0584e443c0ef90a82cbbed2
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2014-09-05 16:02:35 +0200

    Added KDE and Xfce books

--- packages/update-packages.py
+++ packages/update-packages.py
@@ -8,25 +8,31 @@ languages = {
             'pt-BR':'pt_BR'
             }
 
-os.system("rm -rf mint-user-guide/mint-user-guide-cinnamon/*")
-os.system("rm -rf mint-user-guide/mint-user-guide-mate/*")
+books = ['Cinnamon', 'Mate', 'KDE', 'Xfce']
+
+os.system("rm -rf mint-user-guide/mint-user-guide-*/*")
 
 base_dir = os.getcwd()
 
 for language in languages.keys():
 	if os.path.exists("../books/User_Guide/tmp/%s" % language):
 		yelp_language = languages[language]
-
-		os.system("mkdir -p mint-user-guide/mint-user-guide-cinnamon/usr/share/help/%s/linuxmint/" % yelp_language)
-		os.system("mkdir -p mint-user-guide/mint-user-guide-mate/usr/share/help/%s/linuxmint/" % yelp_language)
 		
-		for book in ['Cinnamon', 'Mate']:
+		for book in books:						
 			source_dir = "%s/../books/User_Guide/tmp/%s/xml" % (base_dir, language)
 			dest_dir = "%s/mint-user-guide/mint-user-guide-%s/usr/share/help/%s/linuxmint/" % (base_dir, book.lower(), yelp_language)
+			os.system("mkdir -p %s" % dest_dir)
 			os.chdir(dest_dir)
 
-			for content in ['Common_Content', 'images', 'Book_Info.xml', 'user-guide-%s-*' % book.lower(), 'user-guide-common-*']:
+			# copy common chapters
+			for content in ['Common_Content', 'images', 'Book_Info.xml', 'user-guide-common-*']:
 				os.system("cp -R %s/%s ./" % (source_dir, content))
+
+			# copy DE specific chapters if any
+			try:
+				os.system("cp -R %s/user-guide-%s-* ./ 2> /dev/null" % (source_dir, book.lower()))
+			except:
+				pass
 			
 			# copy the index
 			os.system("cp %s/%s_User_Guide.xml ./index.docbook" % (source_dir, book))
@@ -41,8 +47,9 @@ for language in languages.keys():
 			# Replace the EDITION to the correct value
 			os.system("grep -r -l '<!ENTITY EDITION \".*\">' * | xargs sed -i 's/<!ENTITY EDITION \".*\">/<!ENTITY EDITION \"%s\">/g'" % book)			
 
-# clean up images
-os.system("rm -rf mint-user-guide/mint-user-guide-cinnamon/usr/share/help/*/linuxmint/images/desktops/mate")
-os.system("rm -rf mint-user-guide/mint-user-guide-mate/usr/share/help/*/linuxmint/images/desktops/cinnamon")
+			# clean up images
+			for name in books:
+				if book != name:
+					os.system("rm -rf images/desktops/%s" % name)
 
 os.chdir(base_dir)
